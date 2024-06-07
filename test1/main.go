349fa77b93f531980aeebcf5e69927b84b567a88
tassadly <tassadly@L02R01S01s-iMac.local>
2024-06-08 01:39:33 +0500

    test1

---
+++ test1/main.go
@@ -0,0 +1,44 @@
+package main
+
+func main() {
+	// y := make([]int, 4)
+	// y = append(y, 4)
+	// fmt.Println(y[3])
+
+	// var t []int      // y является nil срезом
+	// t = append(t, 4) // добавляем элемент 4 в nil срез
+	// fmt.Println(t)
+
+	// // Создаем срез длиной 3 и емкостью 5
+	// s := make([]int, 3, 4)
+	// fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+
+	// // Заполняем элементы среза
+	// s[0] = 1
+	// s[1] = 2
+	// s[2] = 3
+	// fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+
+	// // Добавляем новый элемент
+	// s = append(s, 4)
+	// fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+
+	// // Добавляем еще один элемент
+	// s = append(s, 5)
+	// fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+
+	// // Добавляем элемент, превышающий текущую емкость
+	// s = append(s, 6)
+	// fmt.Printf("s: %v, len: %d, cap: %d\n", s, len(s), cap(s))
+
+	// a := 10
+	// fmt.Printf("%d\n", &a)
+
+	// websites := make(map[string]float64)
+
+	// websites["itProger"] = 0.8
+	// websites["Yandex"] = 0.99
+
+	// fmt.Println(websites["itProger"])
+
+}
